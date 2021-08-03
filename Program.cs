169aa44994fe955ba537df786285e8f51ebd19cf
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-03 22:15:06 +0530

    feat:Welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome line comparison computation program");
+        }
+    }
+}
