2551a4b700bfa9203bb556eff1f4d6c8ba556f7d
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-03 23:07:14 +0530

    feat:lengthOfLine

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace lineComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome line comparison computation program");
+            int x1, x2, y1, y2;
+            x1 = 12;
+            x2 = 13;
+            y1 = 11;
+            y2 = 10;
+            var distance = Math.Sqrt((Math.Pow(x2 - x1 , 2) + Math.Pow(y2 - y1 , 2)));
+            Console.WriteLine(distance);  
         }
     }
 }
